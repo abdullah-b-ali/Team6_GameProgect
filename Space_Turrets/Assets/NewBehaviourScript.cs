7638a9aa71a82abdd51e8ec4542cc688b54533e9
Evan Williams <edw@evanwilliams.info>
2015-11-10 10:06:15 -0500

    added a system to control the movement o the enemies

---
+++ Space_Turrets/Assets/NewBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public GameObject Enemy1, Enemy2;
+    public Transform startMarker;
+    public Transform endMarker;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+    public bool move = false;
+    // Use this for initialization
+    void Start () {
+
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+    }
+    public void OnClick()
+    {
+
+    }
+
+    public void StartAnim()
+    {
+        move = true;
+    }
+    // Update is called once per frame
+    void Update () {
+        if (move == true)
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            Enemy1.transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+        }
+
+    }
+}
