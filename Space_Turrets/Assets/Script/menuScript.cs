63ee7df92acda40b58e9463d141024b78c3593c3
B00M5T1CK <fpagalos@gmail.com>
2015-12-06 11:54:07 -0500

    added menu

---
+++ Space_Turrets/Assets/Script/menuScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+	
+	public Canvas quitMenu;
+	public Button startText;
+	public Button exitText;
+	public Button storyText;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		storyText = storyText.GetComponent<Button> ();
+		quitMenu.enabled = false;
+	}
+	
+	public void ExitPress()
+	{
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+	
+	public void NoPress()
+	{
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+	
+	public void StartLevel()
+	{
+		Application.LoadLevel(1);
+	}
+
+	public void StartStory()
+	{
+		Application.LoadLevel(2);
+	}
+	
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
